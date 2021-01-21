a6949e3bf8cf5574715c35bd507f2a99b95eada0
mickey <qq523107430@163.com>
2021-01-21 17:45:33 +0800

    filter 影响性能

--- Week_03/netty-gateway/src/main/java/io/github/mickey/filter/both/TimeFilter.java
+++ Week_03/netty-gateway/src/main/java/io/github/mickey/filter/both/TimeFilter.java
@@ -5,27 +5,25 @@ import io.netty.handler.codec.http.HttpRequest;
 import io.netty.handler.codec.http.HttpResponse;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 @Slf4j
 public class TimeFilter implements PreAndPostFilter {
-    private static final AtomicLong start = new AtomicLong();
+//    private static final AtomicLong start = new AtomicLong();
 
     @Override
     public void preFilter(HttpRequest request, ChannelHandlerContext ctx) {
-        final long s = System.currentTimeMillis();
-        start.set(s);
-        log.info("request start:{}", start.get());
+//        final long s = System.currentTimeMillis();
+//        start.set(s);
+//        log.info("request start:{}", start.get());
     }
 
 
     @Override
     public void postFilter(HttpResponse response, ChannelHandlerContext ctx) {
-        log.info("response start:{}", start.get());
-        final long end = System.currentTimeMillis();
-        log.info("response end:{}", end);
-        log.info("proxy request time:{} ms", end - start.get());
-        start.set(0);
+//        log.info("response start:{}", start.get());
+//        final long end = System.currentTimeMillis();
+//        log.info("response end:{}", end);
+//        log.info("proxy request time:{} ms", end - start.get());
+//        start.set(0);
     }
 
 }
