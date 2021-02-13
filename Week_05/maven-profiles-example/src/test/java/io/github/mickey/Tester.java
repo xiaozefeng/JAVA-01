c4f5dd1037589c18ed950d2ab9e6a97bf7e63d19
xiaozefeng <qq523107430@163.com>
2021-02-13 10:15:42 +0800

    maven profile eample 1

---
+++ Week_05/maven-profiles-example/src/test/java/io/github/mickey/Tester.java
@@ -0,0 +1,15 @@
+package io.github.mickey;
+
+import org.junit.Test;
+
+/**
+ * @author mickey
+ * @date 2/12/21 23:08
+ */
+public class Tester {
+
+    @Test
+    public void test() throws Exception{
+        System.out.println("test running....");
+    }
+}
